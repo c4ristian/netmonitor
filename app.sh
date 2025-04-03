00e9eec882d4ed82ed4ba701db6cbfd9520333a2
c4ristian <christian.koch@gmx.li>
2025-04-03 18:07:51 +0200

    Warnings suppressed

--- app.sh
+++ app.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 # This script runs the netmonitor application in its conda environment.
-conda run --no-capture-output -n netmonitor python app.py
+# Warnings are suppressed by redirecting stderr to /dev/null.
+# To debug the application run app.py directly.
+conda run --no-capture-output -n netmonitor python app.py 2>/dev/null
